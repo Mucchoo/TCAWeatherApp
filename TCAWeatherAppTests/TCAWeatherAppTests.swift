9f3f46b9b0cf0f0c1d33eb3811941d5e06186056
Musa <yazujumusa@gmail.com>
2025-02-13 10:18:36 +0900

    Add test file

---
+++ TCAWeatherAppTests/TCAWeatherAppTests.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherFeatureTests.swift
+//  TCAWeatherAppTests
+//
+//  Created by Musa Yazici on 2/13/25.
+//
+
+import XCTest
+import ComposableArchitecture
+@testable import TCAWeatherApp
+
+@MainActor
+final class WeatherFeatureTests: XCTestCase {
+
+}
